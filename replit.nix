c277520836b8757726c52e7abe92f0792a7c3906
sajtcraft <83774744+sajtcraft@users.noreply.github.com>
2024-12-12 18:29:03 +0100

    Add files via upload

---
+++ replit.nix
@@ -0,0 +1,16 @@
+{ pkgs }: {
+    deps = [
+        pkgs.wget
+        pkgs.bashInteractive
+        pkgs.man
+        pkgs.jq
+        pkgs.ffmpeg
+        pkgs.jre8
+        pkgs.jdk11
+        pkgs.git
+        pkgs.tmux
+        pkgs.wget
+        pkgs.curl
+        pkgs.dialog
+    ];
+}
